683d08bea05c93bb08ffebcd24e3c92fca16e818
originaltebas <originaltebas@gmail.com>
2020-11-03 13:46:44 +0100

    todo

---
+++ js/multiLang.js
@@ -0,0 +1,61 @@
+var MultiLang = function(path, name, lang, onload) {
+    // variables
+    this.phrases = {};
+
+    // keep only first two chareacters, for example 'en-US', 'fr', 'nl-NL', 'it', 'zh' etc.
+    this.selectedLanguage = (lang || navigator.language || navigator.userLanguage).substring(0, 5);
+
+    // load json from url
+    if (typeof path != 'undefined') {
+        if (typeof name != 'undefined') {
+            if (typeof lang != 'undefined') {
+                var req = new XMLHttpRequest();
+
+                req.onreadystatechange = function() {
+
+                    if (req.readyState == 4) {
+                        // load translations
+                        this.phrases = JSON.parse(req.responseText);
+                    }
+                }
+
+                req.open("GET", path + "/" + name + "." + lang + ".json", true);
+                req.send();
+            }
+        }
+    }
+
+    this.changeLanguage = function(langcode) {
+
+        // check if language code exists in translations
+        if (!this.phrases.hasOwnProperty(langcode)) {
+            // if it doesn't exist; default to first language 
+
+            // NOTE: the order of properties in a JSON object are not *guaranteed* to be the same as loading time,
+            // however in practice all browsers do return them in order
+            for (var key in this.phrases) {
+                if (this.phrases.hasOwnProperty(key)) {
+                    langcode = key;
+                    break;
+                };
+            };
+        };
+
+        // set as selected language code
+        this.selectedLanguage = langcode;
+    };
+
+
+    this.get = function(key) {
+        // get key phrase
+        var str;
+
+        // check if any languages were loaded
+        if (this.phrases[this.selectedLanguage]) str = this.phrases[this.selectedLanguage][key];
+
+        // if key does not exist, return the literal key
+        str = (str || key);
+
+        return str;
+    };
+}
